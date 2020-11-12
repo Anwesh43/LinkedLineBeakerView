e5efba6efcc96c6986fdae21e0f8787b61931863
Anwesh43 <anweshthecool0@gmail.com>
2020-11-13 03:04:06 +0530

    adding LBNode class

--- linebeakerview/src/main/java/com/example/linebeakerview/LineBeakerView.kt
+++ linebeakerview/src/main/java/com/example/linebeakerview/LineBeakerView.kt
@@ -136,4 +136,45 @@ class LineBeakerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBNode(var i : Int, val state : State = State()) {
+
+        private var next : LBNode? = null
+        private var prev : LBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBNode {
+            var curr : LBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
